1b88027aaba709f4973e337fd948217ed27f042b
serafino.pandolfini <serafino.pandolfini@studio.unibo.it>
2023-02-27 11:48:42 +0100

    test circular list size

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,19 +1,35 @@
 import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
+    private static final int INITIAL_SIZE = 3;
     CircularList circularList;
+    CircularList emptyCircularList;
 
     @BeforeEach
     void beforeEach(){
+        emptyCircularList = new CircularListImpl();
+        circularList = new CircularListImpl();
+        circularList.add(1);
+        circularList.add(2);
+        circularList.add(3);
+    }
 
+    @Test
+    void testSize(){
+        assertEquals(INITIAL_SIZE, circularList.size());
     }
 
     @Disabled
